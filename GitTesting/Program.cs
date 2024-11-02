9b446b8608a6c46323fd294ba1ea7cf2a549d8b2
Jon ryea <jryea80@gmail.com>
2024-11-02 17:15:57 -0600

    Now we have many messages

--- GitTesting/Program.cs
+++ GitTesting/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World2! I'm modifying again!");
+Console.WriteLine("Hello, World2! I'm modifying again!");
+Console.WriteLine("Hello, World2! I'm modifying again!");
+Console.WriteLine("Hello, World2! I'm modifying again!");
+Console.WriteLine("Hello, World2! I'm modifying again!");
+Console.WriteLine("Hello, World2! I'm modifying again!");
 
