9c4dddfd9f7a7a76d4b35164d6da269e620b6058
Jon ryea <jryea80@gmail.com>
2024-11-02 17:33:01 -0600

    Initial Commit

--- GitTesting/Program.cs
+++ GitTesting/Program.cs
@@ -1,8 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World2! I'm modifying again!");
-Console.WriteLine("Hello, World2! I'm modifying again!");
-Console.WriteLine("Hello, World2! I'm modifying again!");
-Console.WriteLine("Hello, World2! I'm modifying again!");
-Console.WriteLine("Hello, World2! I'm modifying again!");
-Console.WriteLine("Hello, World2! I'm modifying again!");
+﻿void MyFirstMethod()
+{
+    // Do something
+}
+
+void MySecondMethod()
+{
+    // Do something
+}
+
+void MyThirdMethod()
+{
+    // Do something
+}
 
